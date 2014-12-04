94544c87f964791840784f7d5b6fc70bf58b3585
Deepak Sharma <sharma.deepak2@mayo.edu>
2014-12-04 15:12:45 -0600

    AML MD Plugin

---
+++ AMLMDPlugin/src/main/java/edu/mayo/aml/tooling/plugin/MDPluginMain.java
@@ -0,0 +1,35 @@
+package edu.mayo.aml.tooling.plugin;
+
+import com.nomagic.magicdraw.plugins.Plugin;
+
+/**
+ * Created by dks02 on 12/4/14.
+ */
+public class MDPluginMain extends Plugin
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I am AML Plugin running from Main Method." );
+    }
+
+    @Override
+    public void init()
+    {
+        javax.swing.JOptionPane.showMessageDialog(null, "AML MD Plugin init");
+        System.out.print("my plugin got executed");
+    }
+
+    @Override
+    public boolean close()
+    {
+        javax.swing.JOptionPane.showMessageDialog(null, "AML MD Plugin close");
+        return true;
+    }
+
+    @Override
+    public boolean isSupported()
+    {
+        return true;
+    }
+}
